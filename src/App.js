4fd99e03ba3c90b89653c289c14586bdc0cc80db
Dominic Njokuocha <dommyn4u@gmail.com>
2022-11-07 16:18:49 +0100

    new file

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+import './App.css';
+
+const validPhoneNumber = /^\d{1,10}$/;
+
+export default function PhoneNumber() {
+  const [phone, setPhone] = useState('');
+  
+   const handleChange = ({ target })=> {
+     const newPhone = target.value;
+     const isValid = validPhoneNumber.test(newPhone);
+     if (isValid) {
+       setPhone(newPhone);
+     }
+     // just ignore the event, when new value is invalid
+    };
+
+  return (
+    <div className='phone'>
+      <label for='phone-input'>Phone: </label>
+      <input value={phone} onChange={handleChange} id='phone-input' />
+    </div>
+  );
+}
+
